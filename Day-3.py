c7f765d7e02a1820100ed5d213a59b25b3612128
steinerML <102678863+steinerML@users.noreply.github.com>
2022-04-14 18:13:25 +0200

    Add files via upload

---
+++ Day-3.py
@@ -0,0 +1,12 @@
+
+# We take input from user as an integer
+
+score = int(input("Enter your exam result: "))
+
+if score > 100 or = 0
+    print("Score is not valid")
+
+elif score >= 50:
+    print("Congratulations! You passed the exam")
+else :
+    print("Sorry, you have failed miserably!")
