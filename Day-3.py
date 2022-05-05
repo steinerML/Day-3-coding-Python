dff05b17ae599aee1ac67d0e966ebe0ac14f830d
steinerML <102678863+steinerML@users.noreply.github.com>
2022-05-05 15:56:14 +0200

    Update Day-3.py

--- Day-3.py
+++ Day-3.py
@@ -6,7 +6,7 @@ score = int(input("Enter your exam result: "))
 if score > 100 or = 0
     print("Score is not valid")
 
-elif score >= 50:
+elif score >= 50
     print("Congratulations! You passed the exam")
 else :
     print("Sorry, you have failed miserably!")
